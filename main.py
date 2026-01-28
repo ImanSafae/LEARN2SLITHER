b1f1581f1682c208a57ef93b077cb91ac5abb9f6
Iman <iman.taouil@amadeus.com>
2026-01-28 23:37:41 +0100

    board visualization - early steps

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+import pygame
 from snake.Game import Game
+from enum import Enum
+
+class Colors(Enum):
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+
+
+
+def init_window(game: Game):
+    pygame.init()
+    surface: pygame.Surface = pygame.display.set_mode((550, 550))
+
+    exit_game = False
+    surface.fill(Colors.WHITE.value)
+    rect_width = 500 // game.frame_width
+    rect_height = 500 // game.frame_height
+    rect = pygame.Rect(25, 25, rect_width, rect_height)
+    pygame.draw.rect(surface, Colors.BLACK.value, rect, width=1)
+    for j in range(game.frame_height):
+        for i in range(game.frame_width):
+            rect = pygame.Rect(25 + i * rect_width, 25 + j * rect_height, rect_width, rect_height)
+            pygame.draw.rect(surface, Colors.BLACK.value, rect, width=1)
+    pygame.display.update()
+    while not exit_game:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit_game = True
+
+
+        # pygame.display.update()
 
 if __name__ == "__main__":
     game = Game()
+    print("------------------ BOARD ------------------")
     game.print_board()
+    init_window(game)
--- snake/Game.py
+++ snake/Game.py
@@ -1,21 +1,39 @@
 import numpy as np
 import random
+from enum import Enum
+
+class Directions(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+class Statuses(Enum):
+    RUNNING = 0
+    GAME_OVER = 1
 
 class Game():
     width: int
     height: int
+    frame_width: int
+    frame_height: int
     board: list[int]
     snake: list[int]
+    current_direction: Directions
+    status: Statuses
 
     def __init__(self, width: int = 10, height: int = 10):
         self.width = width
         self.height = height
-        self.board = np.zeros((self.width + 2) * (self.height + 2), dtype=int)
+        self.frame_width = self.width + 2
+        self.frame_height = self.height + 2
+        self.board = np.zeros(self.frame_width * self.frame_height, dtype=int)
         print(f"Initialized board of size {self.width}x{self.height}: {len(self.board)}")
         self.__init_walls()
         self.__init_snake()
         self.__init_apples()
-
+        self.current_direction = Directions.UP
+        self.status = Statuses.RUNNING
     def __init_walls(self):
         frame_width: int = self.width + 2
         frame_height: int = self.height + 2
@@ -64,4 +82,20 @@ class Game():
             row = self.board[i * frame_width:(i + 1) * frame_width]
             print(" ".join(str(cell) if cell == 0 else chr(cell) for cell in row))
 
+    def up(self):
+        current_head_position = self.snake[0]
+        new_head_position = current_head_position - self.frame_width
+        if self.current_direction in [Directions.DOWN, Directions.UP]:
+            pass
+        elif self.board[new_head_position] is 0:
+            self.update_snake_position(current_head_position, new_head_position)
+        else:
+            self.check_collisions(current_head_position, new_head_position)
+
+    def update_snake_position(self, current_head_position: int, new_head_position: int):
+        pass
+
+    def check_collisions(self, current_head_position: int, new_head_position: int):
+        if self.board[new_head_position] == ord('W'):
+            self.status = Statuses.GAME_OVER 
     
